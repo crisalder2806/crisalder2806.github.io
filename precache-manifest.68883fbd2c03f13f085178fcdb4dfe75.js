37ad7327ac3dbe2a80cf42dd749f09488b85787a
VuLDN <lednguyenvu@gmail.com>
2019-07-22 01:52:46 +0700

    Updates

---
+++ precache-manifest.68883fbd2c03f13f085178fcdb4dfe75.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e868c64c53742a26a6c700cad0762af",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2c6f63b9736595453c50",
+    "url": "/static/css/main.3bcc7bba.chunk.css"
+  },
+  {
+    "revision": "4b76a9995a8c776b90f5",
+    "url": "/static/js/2.0d91da3d.chunk.js"
+  },
+  {
+    "revision": "2c6f63b9736595453c50",
+    "url": "/static/js/main.aca492fc.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
